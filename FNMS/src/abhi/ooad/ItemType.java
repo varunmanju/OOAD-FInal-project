ed5f55683edc5f5cf258323c4408a2ba192ab5d0
Alexa Rinard <alri8917@colorado.edu>
2022-03-04 12:20:10 -0700

    Adding prof's code

---
+++ FNMS/src/abhi/ooad/ItemType.java
@@ -0,0 +1,8 @@
+package com.OOAD;
+
+// I ended up using this enum a lot!
+// I essentially used it instead of bothering with a name
+// by printing it out in lowercase
+public enum ItemType {
+    PAPERSCORE,CD,VINYL,GUITAR,BASS,MANDOLIN,FLUTE,HARMONICA;
+}
